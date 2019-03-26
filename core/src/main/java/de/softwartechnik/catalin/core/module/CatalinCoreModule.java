c1b4befda4ab150b450e1f577fa8ad631c9f0ed4
Felix Klauke <info@felix-klauke.de>
2019-03-26 12:17:30 +0100

    Add lombok.
    Signed-off-by: Felix Klauke <info@felix-klauke.de>

---
+++ core/src/main/java/de/softwartechnik/catalin/core/module/CatalinCoreModule.java
@@ -0,0 +1,12 @@
+package de.softwartechnik.catalin.core.module;
+
+import com.google.inject.AbstractModule;
+
+public class CatalinCoreModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+
+
+    }
+}
