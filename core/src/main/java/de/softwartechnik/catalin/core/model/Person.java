73f29b85c5c0ce11acc9a0b3b7d86ed065b7cb6d
Felix Klauke <info@felix-klauke.de>
2019-05-11 11:42:16 +0200

    Remove base model for now.

--- core/src/main/java/de/softwartechnik/catalin/core/model/Person.java
+++ core/src/main/java/de/softwartechnik/catalin/core/model/Person.java
@@ -1,14 +1,14 @@
 package de.softwartechnik.catalin.core.model;
 
-import de.softwartechnik.catalin.core.model.base.BaseModel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 import java.util.Date;
 
@@ -17,9 +17,12 @@ import java.util.Date;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode(callSuper = true)
-@ToString(callSuper = true)
-public class Person extends BaseModel {
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
 
     @Column(name = "first_name", nullable = false)
     private String firstName;
