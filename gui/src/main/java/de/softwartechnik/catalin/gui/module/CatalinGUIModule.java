db73e62f6088ee12895952c7e97d90e0d8769360
Felix Klauke <info@felix-klauke.de>
2019-05-15 21:40:12 +0200

    Add i18n support via resource bundles.

--- gui/src/main/java/de/softwartechnik/catalin/gui/module/CatalinGUIModule.java
+++ gui/src/main/java/de/softwartechnik/catalin/gui/module/CatalinGUIModule.java
@@ -1,13 +1,19 @@
 package de.softwartechnik.catalin.gui.module;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
 import de.softwartechnik.catalin.gui.CatalinGUI;
 import de.softwartechnik.catalin.gui.swing.CatalinSwingGUI;
 import de.softwartechnik.catalin.core.config.CatalinCoreConfig;
 import de.softwartechnik.catalin.core.module.CatalinCoreModule;
 
+import javax.inject.Named;
+import javax.inject.Singleton;
+import java.util.ResourceBundle;
+
 public class CatalinGUIModule extends AbstractModule {
 
+    private static final String RESOURCE_BUNDLE_NAME = "catalin";
     private final CatalinCoreConfig catalinCoreConfig;
 
     public CatalinGUIModule(CatalinCoreConfig catalinCoreConfig) {
@@ -23,5 +29,13 @@ public class CatalinGUIModule extends AbstractModule {
         install(new CatalinCoreModule(catalinCoreConfig));
 
         bind(CatalinGUI.class).to(CatalinSwingGUI.class);
+    }
+
+    @Provides
+    @Singleton
+    @Named("i18n")
+    public ResourceBundle provideI18nResourceBundle() {
+
+        return ResourceBundle.getBundle(RESOURCE_BUNDLE_NAME);
     }
 }
