91f09a1950fde3de221111c9f42ef237677704cb
Felix Klauke <info@felix-klauke.de>
2019-05-28 09:19:25 +0200

    Add sonarqube support.

---
+++ core/src/test/java/de/softwartechnik/catalin/core/repository/map/AbstractMapRepositoryTest.java
@@ -0,0 +1,29 @@
+package de.softwartechnik.catalin.core.repository.map;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AbstractMapRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void find() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void remove() {
+    }
+}
