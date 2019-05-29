4215ad3f2df138e3df391f2ed362dc53383c6f18
LenaW <moccaleni@googlemail.com>
2019-05-29 22:30:20 +0200

    add test file for row filter

---
+++ gui/src/test/java/de/softwartechnik/catalin/gui/swing/controller/CatalinRowFilterTest.java
@@ -0,0 +1,12 @@
+package de.softwartechnik.catalin.gui.swing.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CatalinRowFilterTest {
+
+    @Test
+    void include() {
+    }
+}
