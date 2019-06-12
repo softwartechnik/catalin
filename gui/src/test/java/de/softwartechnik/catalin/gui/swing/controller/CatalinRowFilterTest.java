54e61fe88698d8f7390e2a6017bf5f22eb529d54
LenaW <moccaleni@googlemail.com>
2019-06-12 12:29:36 +0200

    modify test file for row filter

--- gui/src/test/java/de/softwartechnik/catalin/gui/swing/controller/CatalinRowFilterTest.java
+++ gui/src/test/java/de/softwartechnik/catalin/gui/swing/controller/CatalinRowFilterTest.java
@@ -1,10 +1,23 @@
 package de.softwartechnik.catalin.gui.swing.controller;
 
+import de.softwartechnik.catalin.core.model.CatalinModel;
+import de.softwartechnik.catalin.core.repository.map.AbstractMapRepository;
+import de.softwartechnik.catalin.gui.swing.model.BookingsTableModel;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import javax.swing.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CatalinRowFilterTest {
+
+    private static final String SEARCH = "3";
+    CatalinRowFilter rowFilter;
+    @BeforeEach
+    void setUp() {
+      rowFilter = new CatalinRowFilter(SEARCH);
+    }
 
     @Test
     void include() {
